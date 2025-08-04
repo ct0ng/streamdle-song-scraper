c6b35623b9c734057fefccf2a31418a933a47cd9
Chris Tong <ct1799@nyu.edu>
2025-08-04 17:58:05 -0400

    use different filename for logging and fix gitignore

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.scraper import scrape_artist_data, scrape_song_data
 from src.db_utils import upsert_artist_data, upsert_song_data
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='basic.log',encoding='utf-8',level=logging.INFO, filemode = 'w', format='%(process)d-%(levelname)s-%(message)s')
+logging.basicConfig(filename='scraper.log',encoding='utf-8',level=logging.INFO, filemode = 'w', format='%(process)d-%(levelname)s-%(message)s')
 
 def main():
     """
