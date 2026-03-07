18f5e7eff795e330c6fdcc25915f84327823da5c
Chris Tong <ct1799@nyu.edu>
2026-03-07 14:23:58 -0500

    add util method for inserting album data into db

--- src/db_utils.py
+++ src/db_utils.py
@@ -52,6 +52,53 @@ def query_artist_data():
 
     return artist_data
 
+def insert_album_data(album_data):
+    if not album_data:
+        return {}
+
+    logger.info(f'Upserting data for {len(album_data)} albums...')
+
+    conn = setup_database()
+    cursor = conn.cursor()
+
+    # stores mapping of (album_title, cover_url) to album_id
+    album_id_map = {}
+
+    for album_title, cover_url in album_data:
+        # check if album already exists with same title and cover_url
+        check_query = """
+            SELECT album_id FROM public.album 
+            WHERE title = %s AND (cover_url = %s OR (cover_url IS NULL AND %s IS NULL))
+            LIMIT 1;
+        """
+        cursor.execute(check_query, (album_title, cover_url, cover_url))
+        result = cursor.fetchone()
+        if result:
+            # use existing ID if album exists
+            album_id = result[0]
+        else:
+            # otherwise, insert new album
+            insert_query = """
+                INSERT INTO public.album (title, cover_url, created_datetime)
+                VALUES (%s, %s, NOW())
+                RETURNING album_id;
+            """
+            cursor.execute(insert_query, (album_title, cover_url))
+            album_id = cursor.fetchone()[0]
+
+        album_id_map[(album_title, cover_url)] = album_id
+
+    conn.commit()
+
+    logger.info('Closing database connection')
+
+    cursor.close()
+    conn.close()
+
+    logger.info("Updated album table data")
+
+    return album_id_map
+
 def upsert_song_data(song_data):
 
     logger.info(f'Upserting data for {len(song_data)} songs...')
@@ -60,9 +107,12 @@ def upsert_song_data(song_data):
     cursor = conn.cursor()
 
     upsert_query = """
-        INSERT INTO public.song (title, artist_id, stream_count, created_datetime)
-        VALUES (%s, %s, %s, NOW())
-        ON CONFLICT (title, artist_id) DO UPDATE SET stream_count = EXCLUDED.stream_count, updated_datetime = NOW();
+        INSERT INTO public.song (title, artist_id, stream_count, album_id, created_datetime)
+        VALUES (%s, %s, %s, %s, NOW())
+        ON CONFLICT (title, artist_id) DO UPDATE SET 
+            stream_count = EXCLUDED.stream_count, 
+            album_id = EXCLUDED.album_id,
+            updated_datetime = NOW();
     """
     execute_batch(cursor, upsert_query, song_data, page_size=100)
     conn.commit()
