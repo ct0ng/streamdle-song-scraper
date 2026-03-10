b0dc8086836eba98c146ce86c6f9f20d89386e37
Chris Tong <ct1799@nyu.edu>
2026-03-10 15:12:04 -0400

    update config to include more popular songs

--- src/config.py
+++ src/config.py
@@ -18,11 +18,11 @@ SCRAPER_CONFIG = {
 }
 
 # Define the ranges for the number of songs to scrape based on the artist's total stream ranking
-# E.g. an artist that's in the top 100 will have 25 songs scraped
+# E.g. an artist that's in the top 100 will have 40 songs scraped
 SONG_RANGES = {
-    range(2000, 3000): 5,
-    range(1000, 2000): 10,
-    range(500, 1000): 15,
-    range(100, 500): 20,
-    range(0, 100): 25,
+    range(2000, 3000): 10,
+    range(1000, 2000): 20,
+    range(500, 1000): 30,
+    range(100, 500): 35,
+    range(0, 100): 40,
 }
